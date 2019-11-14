882146fe8bcce84a2227f03c32829fec6da8e8b9
Paul Portier <paulportier@gmail.com>
2019-11-14 11:48:00 +0100

    setup complete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+
+// app/javascript/packs/application.js
+import 'bootstrap';
