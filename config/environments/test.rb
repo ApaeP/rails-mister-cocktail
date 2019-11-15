94098fde583df61c3f49a0bac51fabbdfccd285e
Paul Portier <paulportier@gmail.com>
2019-11-15 16:23:17 +0100

    mmmmmmh

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,6 +17,8 @@ Rails.application.configure do
   config.public_file_server.headers = {
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
+    config.active_storage.service = :cloudinary
+
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
